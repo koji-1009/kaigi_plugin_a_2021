ea091f3983fa5efbbaa59b2cff43583d8754b6f4
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-11-10 08:55:52 +0900

    Update demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: lightTheme,
       darkTheme: darkTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'kaigi plugin a 2021'),
     );
   }
 }
